e5e7dafc70d6b81d1d2e2e956fce029ecd30413c
certifiedTboy <etosin70@gmail.com>
2025-04-05 13:30:03 +0100

    update module

--- src/lib/ifa/model.ts
+++ src/lib/ifa/model.ts
@@ -1,7 +1,7 @@
 import { MongoClient } from "mongodb";
 
 export class Ifa {
-  constructor(connectString: string, dbName: string) {
+  constructor(connectString: string, dbName?: string) {
     (global as any).dbData = {
       connectString: dbName ? `${connectString}${dbName}` : connectString,
       dbName: dbName,

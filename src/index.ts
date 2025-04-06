26e3914041685734433812e37179760ed3495fbb
certifiedTboy <etosin70@gmail.com>
2025-04-06 06:44:30 +0100

    update module

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
-export * from "./lib/ifa/model";
-export * from "./lib/ifa/connect";
-export * from "./lib/ifa/schema";
+import { Ifa } from "./lib/ifa/model";
+import { connect } from "./lib/ifa/connect";
+import { Schema } from "./lib/ifa/schema";
+
+export { Ifa, connect, Schema };

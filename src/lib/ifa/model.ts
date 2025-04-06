a46caef746829579ca8da7358547a521ec6c2b8e
certifiedTboy <etosin70@gmail.com>
2025-04-06 06:35:32 +0100

    update module

--- src/lib/ifa/model.ts
+++ src/lib/ifa/model.ts
@@ -1,19 +1,21 @@
 import { MongoClient } from "mongodb";
-import { Schema } from "./schema";
 
-export class Ifa extends Schema {
-  constructor(
-    connectString: string,
-    dbName: string,
-    options?: any,
-    collectionName?: string
-  ) {
-    super(collectionName || "", options);
-    console.log("initialize instance of Ifa");
-    (global as any).dbData = {
-      connectString: dbName ? `${connectString}${dbName}` : connectString,
-      dbName: dbName,
-      client: new MongoClient(connectString),
-    };
-  }
-}
+// export class Ifa {
+//   constructor(connectString: string, dbName: string) {
+//     console.log("initialize instance of Ifa");
+//     (global as any).dbData = {
+//       connectString: dbName ? `${connectString}${dbName}` : connectString,
+//       dbName: dbName,
+//       client: new MongoClient(connectString),
+//     };
+//   }
+// }
+
+export const initializeIfa = (connectString: string, dbName: string) => {
+  console.log("initialize instance of Ifa");
+  (global as any).dbData = {
+    connectString: dbName ? `${connectString}${dbName}` : connectString,
+    dbName: dbName,
+    client: new MongoClient(connectString),
+  };
+};

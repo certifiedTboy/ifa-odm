502a33a95e38009adaba6dec29ccd8dae41665c4
certifiedTboy <etosin70@gmail.com>
2025-04-18 13:04:46 +0100

    update schema logic

---
+++ src/example/model.ts
@@ -0,0 +1,31 @@
+import { Schema } from "../lib/ifa/schema";
+
+const user = new Schema(
+  "users",
+  {
+    firstName: { type: "string", required: true },
+    lastName: { type: "string", required: true },
+    age: { type: "number", required: true },
+    email: { type: "string", unique: true },
+    isGraduated: { type: "bool", required: true },
+    records: [
+      {
+        score: { type: "number" },
+        cgpa: { type: "number" },
+        minItems: 1,
+        maxItems: 5,
+      },
+    ],
+    address: {
+      street: { type: "string" },
+      city: { type: "string" },
+      state: { type: "string" },
+      zipCode: { type: "string" },
+      zip: [{ type: "string" }],
+    },
+    hobbies: [{ type: "string", minItems: 1, maxItems: 5 }],
+  },
+  { timestamps: true }
+);
+
+export default user;

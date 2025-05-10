5e2f009f0b3a21c2cfce127e93cde6495cc27aa5
certifiedTboy <etosin70@gmail.com>
2025-05-10 19:09:20 +0100

    add clear comments

--- src/lib/ifa/schema.ts
+++ src/lib/ifa/schema.ts
@@ -5,6 +5,15 @@ import { SchemaHelper } from "../../helpers/schema";
 import { createCollection } from "./collection";
 import { MongodbError } from "../errors/MongodbError";
 
+/**
+ * @class Schema
+ * @description This class is used to create a new schema for a collection in the database.
+ * It provides methods to create, update, find, and delete documents in the collection.
+ * It also provides methods to create and update collections in the database.
+ * @param {string} collectionName - The name of the collection to be created.
+ * @param {any} options - The properties of the collection to be created.
+ * @param {object} timestamps - The timestamps object is used to create and update the createdAt and updatedAt fields in the collection.
+ */
 export class Schema {
   private collectionName: string;
   private options: any;
@@ -36,6 +45,12 @@ export class Schema {
     };
   }
 
+  /**
+   * @method create
+   * @description This method creates a new document in the collection.
+   * It validates the document before inserting it into the collection.
+   * @param {object} options - The document to be created.
+   */
   create(options: any) {
     try {
       Validator.validateDoc(options);
@@ -85,12 +100,43 @@ export class Schema {
     }
   }
 
+  /**
+   * @method createMany
+   * @description This method creates multiple documents in the collection.
+   * It validates the documents before inserting them into the collection.
+   * @param {array} options - The array of documents to be created.
+   */
   createMany(options: any[]) {
     try {
+      /**
+       * validate options if it is an array
+       * this is to prevent the user from passing an invalid document
+       */
       Validator.validateArrayDoc(options);
+
+      /**
+       * validate each document in the array
+       * this is to prevent the user from passing an invalid document
+       */
       options.forEach((doc) => Validator.validateDoc(doc));
 
       const { client, dbName } = (global as any).dbData;
+
+      /**
+       * create and update collection if it or does not exist before inserting
+       */
+      const promise = new Promise((resolve: any, reject: any) => {
+        try {
+          resolve(
+            createCollection(client, dbName, this.collectionName, this.options)
+          );
+        } catch (error: unknown) {
+          reject(error);
+        }
+      });
+
+      promise.then().catch((error: unknown) => console.log(error));
+
       const docs =
         this.options.createdAt && this.options.updatedAt
           ? SchemaHelper.updateArrayDocTimestamps(options)
@@ -102,6 +148,11 @@ export class Schema {
     }
   }
 
+  /**
+   * @method persist
+   * @description This method updates a document in the collection.
+   * @param {object} options - The document to be updated.
+   */
   persist(options: any) {
     try {
       const { client, dbName } = (global as any).dbData;
@@ -119,33 +170,74 @@ export class Schema {
     }
   }
 
+  /**
+   * @method find
+   * @description This method finds a document in the collection.
+   * It returns an array of documents if no query is provided.
+   * It must be called with the exec method to execute the query.
+   * @param {object} query - The query to be used to find the documents in the collection.
+   */
   find(query: any = {}) {
     this._query = { ...query, struct: "many" };
 
     return this;
   }
 
+  /**
+   * @sort
+   * @description This method sorts the documents in the collection.
+   * It must be called with the exec method to execute the query.
+   * @param {number} sort - Defines the sort order of the documents in the collection.
+   * 1 for ascending order and -1 for descending order.
+   */
   sort(sort: any) {
     this._sort = sort;
     return this;
   }
 
+  /**
+   * @method limit
+   * @description This method limits the number of documents returned in the query.
+   * It must be called with the exec method to execute the query.
+   * @param {number} limit - The maximum number of documents to be returned in the query.
+   */
   limit(limit: number) {
     this._limit = limit;
     return this;
   }
 
+  /**
+   * @method populate
+   * @description This method populates the documents in the collection with data from a reference collection.
+   * It must be called with the exec method to execute the query.
+   * @param {string} field - The field to be populated in the documents.
+   * @param {object} project - It provides which fields to include or exclude from the populated documents.
+   */
   populate(field: string, project?: {}) {
     this._populate.push(field);
     this._project = project;
     return this;
   }
 
+  /**
+   * @method findOne
+   * @description This method finds a single document in the collection.
+   * It returns a single document if no query is provided.
+   * It must be called with the exec method to execute the query.
+   * @param {object} options - The query to be used to find the document in the collection.
+   */
   findOne(options: any) {
     this._query = { ...options, struct: "single" };
     return this;
   }
 
+  /**
+   * @method findOneById
+   * @description This method finds a single document in the collection by its id.
+   * It returns a single document if no query is provided.
+   * It must be called with the exec method to execute the query.
+   * @param {string} id - The id of the document to be found in the collection.
+   */
   findOneById(id: string) {
     if (!id) throw new CustomError("InvalidQuery", "ObjectId is required");
 
@@ -154,6 +246,13 @@ export class Schema {
     return this;
   }
 
+  /**
+   * @method updateOne
+   * @description This method updates a single document in the collection.
+   * It returns the updated document if no query is provided.
+   * @param {object} filter - The query to be used to find the document in the collection.
+   * @param {object} options - The document to be updated.
+   */
   updateOne(filter: any, options: any) {
     try {
       if (!filter)
@@ -178,6 +277,13 @@ export class Schema {
     }
   }
 
+  /**
+   * @method updateOneById
+   * @description This method updates a single document in the collection by its id.
+   * It returns the updated document if no query is provided.
+   * @param {string} id - The id of the document to be updated in the collection.
+   * @param {object} options - The document to be updated.
+   */
   updateOneById(id: ObjectId | string, options: any) {
     try {
       if (!id) throw new CustomError("InvalidQuery", "ObjectId is required");
@@ -199,6 +305,13 @@ export class Schema {
     }
   }
 
+  /**
+   * @method updateMultiple
+   * @description This method updates multiple documents in the collection.
+   * It returns the updated documents if no query is provided.
+   * @param {object} filter - The query to be used to find the documents in the collection.
+   * @param {object} options - The array of documents to be updated.
+   */
   updateMultiple(filter: any, options: any) {
     try {
       if (!filter)
@@ -219,6 +332,12 @@ export class Schema {
     }
   }
 
+  /**
+   * @method removeOne
+   * @description This method removes a single document from the collection.
+   * It returns the deleted document if no query is provided.
+   * @param {object} filter - The query to be used to find the document in the collection to be removed.
+   */
   removeOne(filter: any) {
     try {
       Validator.validateQueryDoc(filter);
@@ -241,6 +360,11 @@ export class Schema {
     }
   }
 
+  /**
+   * @method removeOneById
+   * @description This method removes a single document from the collection by its id.
+   * @param {string | ObjectId} id - The id of the document to be removed from the collection.
+   */
   removeOneById(id: ObjectId | string) {
     try {
       if (!id) throw new CustomError("InvalidQuery", "ObjectId is required");
@@ -257,6 +381,12 @@ export class Schema {
     }
   }
 
+  /**
+   * @method removeMany
+   * @description This method removes multiple documents from the collection.
+   * It returns the deleted documents if no query is provided.
+   * @param {object} filter - The query to be used to find the documents in the collection to be removed.
+   */
   removeMany(filter: any) {
     try {
       if (!filter)
@@ -284,6 +414,12 @@ export class Schema {
     return error;
   }
 
+  /**
+   * @method exec
+   * @description This method executes the query and returns the result.
+   * It must be called after the find, findOne, findOneById, sort, populate, or limit methods to execute the query.
+   *
+   */
   exec(): Promise<any> {
     try {
       const { client, dbName } = (global as any).dbData;
@@ -376,5 +512,7 @@ export class Schema {
     this._query = {};
     this._sort = null;
     this._limit = null;
+    this._populate = [];
+    this._project = null;
   }
 }

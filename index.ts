43f3d235b261fddd72796b24beb49128a566d65d
certifiedTboy <etosin70@gmail.com>
2025-05-14 17:04:25 +0100

    update directory

--- index.ts
+++
@@ -1,5 +0,0 @@
-export * from "./src/lib/ifa/model";
-export * from "./src/lib/ifa/connect";
-export * from "./src/lib/ifa/schema";
-export * from "./src/lib/ifa/collection";
-export * from "./src/lib/ifa/transactions";
---
+++ src/index.ts
@@ -0,0 +1,5 @@
+export * from "./lib/ifa/model";
+export * from "./lib/ifa/connect";
+export * from "./lib/ifa/schema";
+export * from "./lib/ifa/collection";
+export * from "./lib/ifa/transactions";
